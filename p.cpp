d7848f53cb547b20ce09cff519984f22d54e1d93
Mayur <echoofprogress01@gmail.com>
2026-08-04 00:55:28 +0530

    Create p.cpp

---
+++ p.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Includes the input/output library
+
+int main() {
+    // Prints text to the screen
+    std::cout << "Hello, World!" << std::endl; 
+    
+    return 0; // Indicates successful execution
+}
